d4c9a04336f0ee8cd75d8f3e85ce220abed12e49
Dhawal Bera <57316444+DhawalBera@users.noreply.github.com>
2021-04-03 20:10:18 +0530

    Add Offset and animation at remove activity controller.

--- ScrollviewPagination/PaginationManager.m
+++ ScrollviewPagination/PaginationManager.m
@@ -32,6 +32,14 @@
     self.refreshColor = [UIColor whiteColor];
     self.loaderColor = [UIColor whiteColor];
     
+    self.leftRefreshControlWidth = 60;
+    self.righttRefreshControlWidth = 60;
+    self.bottomRefreshControlHeight = 60;
+    
+    self.leftRefreshControlOffset = 60;
+    self.rightRefreshControlOffset = 60;
+    self.bottomRefreshControlOffset = 60;
+    
     scrollView = scrollview;
     [self addScrollviewOffsetObserver];
     
@@ -87,7 +95,7 @@
 }
 
 - (void)addLeftMostControl{
-    UIView *view = [self getViewWithActivityControl:CGRectMake(-60, 0, 60, scrollView.bounds.size.height)];
+    UIView *view = [self getViewWithActivityControl:CGRectMake(-self.leftRefreshControlWidth, 0, self.leftRefreshControlWidth, scrollView.bounds.size.height)];
     
     UIEdgeInsets insets = scrollView.contentInset;
     insets.left = view.frame.size.width;
@@ -97,12 +105,18 @@
 }
 
 - (void)removeLeftMostControl{
-    [leftMostLoader removeFromSuperview];
-    leftMostLoader = nil;
+    [UIView animateWithDuration:0.2 animations:^{
+        UIEdgeInsets insets = self->scrollView.contentInset;
+        insets.left = 0;
+        self->scrollView.contentInset = insets;
+    } completion:^(BOOL finished) {
+        [self->leftMostLoader removeFromSuperview];
+        self->leftMostLoader = nil;
+    }];
 }
 
 - (void)addRightMostControl{
-    UIView *view = [self getViewWithActivityControl:CGRectMake(scrollView.contentSize.width, 0, 60, scrollView.bounds.size.height)];
+    UIView *view = [self getViewWithActivityControl:CGRectMake(scrollView.contentSize.width, 0, self.righttRefreshControlWidth, scrollView.bounds.size.height)];
     
     UIEdgeInsets insets = scrollView.contentInset;
     insets.right = view.frame.size.width;
@@ -112,12 +126,19 @@
 }
 
 - (void)removeRightMostControl{
-    [rightMostLoader removeFromSuperview];
-    rightMostLoader = nil;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        UIEdgeInsets insets = self->scrollView.contentInset;
+        insets.right = 0;
+        self->scrollView.contentInset = insets;
+    } completion:^(BOOL finished) {
+        [self->rightMostLoader removeFromSuperview];
+        self->rightMostLoader = nil;
+    }];
 }
 
 - (void)addBottomMostControl{
-    UIView *view = [self getViewWithActivityControl:CGRectMake(0, scrollView.contentSize.height, scrollView.bounds.size.width, 60)];
+    UIView *view = [self getViewWithActivityControl:CGRectMake(0, scrollView.contentSize.height, scrollView.bounds.size.width, self.bottomRefreshControlHeight)];
     
     UIEdgeInsets insets = scrollView.contentInset;
     insets.bottom = view.frame.size.height;
@@ -127,8 +148,14 @@
 }
 
 - (void)removeBottomMostControl{
-    [bottomMostLoader removeFromSuperview];
-    bottomMostLoader = nil;
+    [UIView animateWithDuration:0.2 animations:^{
+        UIEdgeInsets insets = self->scrollView.contentInset;
+        insets.bottom = 0;
+        self->scrollView.contentInset = insets;
+    } completion:^(BOOL finished) {
+        [self->bottomMostLoader removeFromSuperview];
+        self->bottomMostLoader = nil;
+    }];
 }
 
 
@@ -137,7 +164,7 @@
     if(!_isStopLeft){
         
         CGFloat offsetX = offsetVal.x;
-        if(offsetX < -100 && !isLoading){
+        if(offsetX < -self.leftRefreshControlOffset && !isLoading){
             isLoading = YES;
             [self addLeftMostControl];
             [self.delegate refreshAll:^(BOOL success) {
@@ -153,7 +180,7 @@
         CGFloat contenWidth = scrollView.contentSize.width;
         CGFloat frameWidth = scrollView.bounds.size.width;
         CGFloat diffX = contenWidth - frameWidth;
-        if (contenWidth > frameWidth && offsetX > (diffX + 130) && !isLoading){
+        if (contenWidth > frameWidth && offsetX > (diffX + self.rightRefreshControlOffset) && !isLoading){
             isLoading = YES;
             [self addRightMostControl];
             [self.delegate loadMore:^(BOOL success) {
@@ -171,7 +198,7 @@
         CGFloat frameHeight = scrollView.bounds.size.height;
         CGFloat diffY = contentHeight - frameHeight;
         
-        if(contentHeight > frameHeight && offsetY > (diffY + 60) && !isLoading){
+        if(contentHeight > frameHeight && offsetY > (diffY + self.bottomRefreshControlOffset) && !isLoading){
             isLoading = YES;
             [self addBottomMostControl];
             [self.delegate loadMore:^(BOOL success) {
